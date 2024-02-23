eb71aa37087e75c97046129e4dcb8bd9703eb4ed
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-23 14:29:51 +0200

    typesript support added, configured, unnecessary contant removed

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <p>JHY</p>;
+};
+
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths(), svgr(), viteCommonjs(), svgr(), viteCommonjs()],
+  server: {
+    port: 3001,
+  },
+  preview: {
+    port: 3001,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+  build: {
+    outDir: 'build',
+  },
+  define: {
+    'process.env.RUNNING_TEST': false,
+  },
+});
