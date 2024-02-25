05a45c60c7883e5dbd4e9eb8db6d1aa77026332a
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-25 21:46:28 +0200

    footer with logo

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,13 +1,14 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 import LayoutContainer from 'components/LayoutContainer';
+import Logo from 'layout/NavBarWithContent/NavBar/Logo';
 import { greyColors, redColors } from 'theme/colors';
 
 const Footer: React.FC = () => {
   return (
     <Box sx={{ display: 'flex', flexShrink: 0 }}>
       <LayoutContainer sx={{ py: { xs: 2, md: 4 }, bgcolor: greyColors.dark, color: redColors.dark }}>
-        <Typography>Footer</Typography>
+        <Logo />
       </LayoutContainer>
     </Box>
   );
