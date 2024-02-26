75310a1f64c689f81c2d0586e2f543eef3dbb8b6
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-26 22:10:06 +0200

    test content added

--- src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
+++ src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
@@ -1,7 +1,22 @@
-import { Box } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+
+import { commonColors } from 'theme/colors';
 
 const ImageContent: React.FC = () => {
-  return <Box>ImageContent</Box>;
+  return (
+    <Stack spacing={4}>
+      <Typography variant='h1' color={commonColors.white}>
+        AUTOKORJAAMO JYVÄSKYLÄN HUOLTOYKKÖSET
+      </Typography>
+      <Typography variant='body1' color={commonColors.white}>
+        Korjaamo Ykkösten Jyväskylän Kuormaajantien autokorjaamo palveluksessasi, tervetuloa paremman autohuollon ja korjaamon asiakkaaksi,
+        jossa ajatellaan kuskin kukkaron ja auton parasta – hintatakuulla tottakai!
+      </Typography>
+      <Typography variant='body2' color={commonColors.white}>
+        Palvelemme arkisin ma – pe 8-16.30 Jyväskylä – Kuormaajantie 6
+      </Typography>
+    </Stack>
+  );
 };
 
 export default ImageContent;
