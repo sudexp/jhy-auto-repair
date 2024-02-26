c63d083b3c26a8f6f0f81de2336bee2b60bdebab
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-26 22:31:40 +0200

    theme config

---
+++ mui.d.ts
@@ -0,0 +1,102 @@
+/* eslint-disable max-lines */
+import '@mui/material/styles/createPalette';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    greyscale: {
+      text: CSS.Properties['color'];
+      light: CSS.Properties['color'];
+      dark: CSS.Properties['color'];
+    };
+    tertiary: {
+      main: CSS.Properties['color'];
+    };
+  }
+
+  interface PaletteOptions {
+    greyscale: {
+      text: CSS.Properties['color'];
+      light: CSS.Properties['color'];
+      dark: CSS.Properties['color'];
+    };
+    tertiary: {
+      main: CSS.Properties['color'];
+    };
+  }
+
+  interface SimplePaletteColorOptions {
+    primary?: CSS.Properties['color'];
+    secondary?: CSS.Properties['color'];
+    tertiary?: CSS.Properties['color'];
+    white?: CSS.Properties['color'];
+    black?: CSS.Properties['color'];
+    red?: CSS.Properties['color'];
+  }
+
+  interface TypographyVariants {
+    display: React.CSSProperties;
+    p1: React.CSSProperties;
+    p2: React.CSSProperties;
+    p3: React.CSSProperties;
+    ui1r: React.CSSProperties;
+    ui1b: React.CSSProperties;
+    ui2r: React.CSSProperties;
+    ui2b: React.CSSProperties;
+    ui3r: React.CSSProperties;
+    ui3b: React.CSSProperties;
+    ui4r: React.CSSProperties;
+    ui4b: React.CSSProperties;
+    ui5r: React.CSSProperties;
+    ui5b: React.CSSProperties;
+    ui6r: React.CSSProperties;
+    ui6b: React.CSSProperties;
+    ui7r: React.CSSProperties;
+    ui7b: React.CSSProperties;
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    display?: React.CSSProperties;
+    p1?: React.CSSProperties;
+    p2?: React.CSSProperties;
+    p3?: React.CSSProperties;
+    ui1r?: React.CSSProperties;
+    ui1b?: React.CSSProperties;
+    ui2r?: React.CSSProperties;
+    ui2b?: React.CSSProperties;
+    ui3r?: React.CSSProperties;
+    ui3b?: React.CSSProperties;
+    ui4r?: React.CSSProperties;
+    ui4b?: React.CSSProperties;
+    ui5r?: React.CSSProperties;
+    ui5b?: React.CSSProperties;
+    ui6r?: React.CSSProperties;
+    ui6b?: React.CSSProperties;
+    ui7r?: React.CSSProperties;
+    ui7b?: React.CSSProperties;
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    display: true;
+    p1: true;
+    p2: true;
+    p3: true;
+    ui1r: true;
+    ui1b: true;
+    ui2r: true;
+    ui2b: true;
+    ui3r: true;
+    ui3b: true;
+    ui4r: true;
+    ui4b: true;
+    ui5r: true;
+    ui5b: true;
+    ui6r: true;
+    ui6b: true;
+    ui7r: true;
+    ui7b: true;
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider } from '@mui/system';
 import { RouterProvider } from 'react-router-dom';
 
+import theme from 'theme';
 import { router } from 'router';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
