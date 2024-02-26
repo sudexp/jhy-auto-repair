6b369de4d949a86a67d5bb810a4c460c7ab5e923
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-26 22:35:32 +0200

    typography modified

--- src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
+++ src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
@@ -5,14 +5,14 @@ import { commonColors } from 'theme/colors';
 const ImageContent: React.FC = () => {
   return (
     <Stack spacing={4}>
-      <Typography variant='h1' color={commonColors.white}>
+      <Typography variant='h2' color={commonColors.white}>
         AUTOKORJAAMO JYVÄSKYLÄN HUOLTOYKKÖSET
       </Typography>
-      <Typography variant='body1' color={commonColors.white}>
-        Korjaamo Ykkösten Jyväskylän Kuormaajantien autokorjaamo palveluksessasi, tervetuloa paremman autohuollon ja korjaamon asiakkaaksi,
-        jossa ajatellaan kuskin kukkaron ja auton parasta – hintatakuulla tottakai!
+      <Typography variant='ui2r' color={commonColors.white}>
+        Korjaamo Huoltoykkösten Jyväskylän Kuormaajantien autokorjaamo palveluksessasi, tervetuloa paremman autohuollon ja korjaamon
+        asiakkaaksi, jossa ajatellaan kuskin kukkaron ja auton parasta – hintatakuulla tottakai!
       </Typography>
-      <Typography variant='body2' color={commonColors.white}>
+      <Typography variant='ui2r' color={commonColors.white}>
         Palvelemme arkisin ma – pe 8-16.30 Jyväskylä – Kuormaajantie 6
       </Typography>
     </Stack>
