00305ce489095a45cea5c2b59bbc2a26a8e53ed2
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-27 21:21:01 +0200

    getBgColor test suite

---
+++ src/layout/Footer/utils.test.ts
@@ -0,0 +1,13 @@
+import { greyColors } from 'theme/colors';
+
+import { getBgColor } from './utils';
+
+describe('Footer getBgColor', () => {
+  it('should return "transparent" if isRootPath is true', () => {
+    expect(getBgColor(true)).toBe('transparent');
+  });
+
+  it('should return dark grey color if isRootPath is false', () => {
+    expect(getBgColor(false)).toBe(greyColors.dark);
+  });
+});
