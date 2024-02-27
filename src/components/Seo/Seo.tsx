a2e85ddf98e2d6abad2c069281e11ff043ceb6f1
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-27 23:49:44 +0200

    Seo component created

---
+++ src/components/Seo/Seo.tsx
@@ -0,0 +1,33 @@
+import { Helmet } from 'react-helmet-async';
+
+interface SeoProps {
+  title: string;
+  description: string;
+  name: string;
+  type: string;
+}
+
+// TODO: check and finnish all meta tags needed
+const Seo: React.FC<SeoProps> = ({ title, description, name, type }: SeoProps) => {
+  return (
+    <Helmet>
+      {/* Standard metadata tags */}
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      {/* End standard metadata tags */}
+      {/* Facebook tags */}
+      <meta property='og:type' content={type} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      {/* End Facebook tags */}
+      {/* Twitter tags */}
+      <meta name='twitter:creator' content={name} />
+      <meta name='twitter:card' content={type} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      {/* End Twitter tags */}
+    </Helmet>
+  );
+};
+
+export default Seo;
