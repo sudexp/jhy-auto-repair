0ef652254e4beee39853ba9c81a3957e4cfd99be
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-28 00:00:29 +0200

    Seo enhanced

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -5,28 +5,36 @@ interface SeoProps {
   description: string;
   name: string;
   type: string;
+  lang?: string;
+  keywords?: string;
+  image?: string;
+  url?: string;
 }
 
-// TODO: check and finnish all meta tags needed
-const Seo: React.FC<SeoProps> = ({ title, description, name, type }: SeoProps) => {
+const Seo: React.FC<SeoProps> = ({ title, description, name, type, lang = 'fi', keywords, image, url }: SeoProps) => {
   return (
-    <Helmet>
-      {/* Standard metadata tags */}
-      <title>{title}</title>
-      <meta name='description' content={description} />
-      {/* End standard metadata tags */}
-      {/* Facebook tags */}
-      <meta property='og:type' content={type} />
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      {/* End Facebook tags */}
-      {/* Twitter tags */}
-      <meta name='twitter:creator' content={name} />
-      <meta name='twitter:card' content={type} />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      {/* End Twitter tags */}
-    </Helmet>
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate={'%s | Jyväskylän Huoltoykköset'}
+      meta={[
+        // Standard metadata tags
+        { name: 'description', content: description },
+        { name: 'keywords', content: keywords },
+        // Facebook tags
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: description },
+        { property: 'og:type', content: type },
+        { property: 'og:image', content: image },
+        // Twitter tags
+        { name: 'twitter:card', content: type },
+        { name: 'twitter:creator', content: name },
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:image', content: image },
+      ]}
+      link={url ? [{ rel: 'canonical', href: url }] : []}
+    />
   );
 };
 
