5469827ee96a62c3836bf98fd4eb27e9408f2711
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-28 23:29:16 +0200

    about page removed

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,7 +3,6 @@ import { createBrowserRouter, createRoutesFromElements, Route } from 'react-rout
 import Layout from 'layout';
 import NotFound from 'pages/NotFound';
 import Root from 'pages/Root';
-import About from 'pages/About';
 import Contacts from 'pages/Contacts';
 import Services from 'pages/Services/Services';
 import Wheels from 'pages/Wheels';
@@ -12,7 +11,6 @@ export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} errorElement={<NotFound />}>
       <Route index element={<Root />} />
-      <Route path='about' element={<About />} />
       <Route path='contacts' element={<Contacts />} />
       <Route path='services' element={<Services />} />
       <Route path='wheels' element={<Wheels />} />
