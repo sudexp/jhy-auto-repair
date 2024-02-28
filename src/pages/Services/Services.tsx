c530853b47a28cb76a81d291cdd386d347053d00
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-28 23:44:54 +0200

    header added

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 import Seo from 'components/Seo';
 
@@ -6,7 +6,9 @@ const Services: React.FC = () => {
   return (
     <>
       <Seo title='Jyväskylän Huoltoykköset - Palvelut' description='' name='' type='' />
-      <Box id='services'>Services</Box>
+      <Stack id='services' spacing={3}>
+        <Typography variant='h4'>TÄYDEN PALVELUN AUTOHUOLTO KUSTANNUSTEHOKKAASTI</Typography>
+      </Stack>
     </>
   );
 };
