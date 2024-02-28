b4a7d6ddbd3dbbc88c3638248a30589abad36770
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-02-28 23:11:01 +0200

    photos added

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 import Seo from 'components/Seo';
 import LeafletMap from 'components/LeafletMap';
@@ -7,6 +7,26 @@ const Contacts: React.FC = () => {
   return (
     <>
       <Seo title='Jyväskylän Huoltoykköset - Yhteystiedot' description='' name='' type='' />
+      <Stack direction='row' spacing={4} sx={{ justifyContent: 'center' }}>
+        <Stack spacing={1}>
+          <Box
+            component='img'
+            sx={{ display: 'block', maxHeight: '200px', maxWidth: '100%' }}
+            alt='Asentaja1'
+            src='/src/assets/png/photo1.png'
+          />
+          <Typography variant='p2'>Asentaja Andrei</Typography>
+        </Stack>
+        <Stack spacing={1}>
+          <Box
+            component='img'
+            sx={{ display: 'block', maxHeight: '200px', maxWidth: '100%', position: 'relative' }}
+            alt='Asentaja2'
+            src='/src/assets/png/photo2.png'
+          />
+          <Typography variant='p2'>Asentaja XXXXXX</Typography>
+        </Stack>
+      </Stack>
       <Box id='contacts'>
         <LeafletMap />
       </Box>
