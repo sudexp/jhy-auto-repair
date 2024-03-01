375cb17ed1e3a71c81ce37dd683f2ef356c29cf4
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-01 16:24:33 +0200

    logo changed

--- src/layout/NavBarWithContent/NavBar/Logo/Logo.tsx
+++ src/layout/NavBarWithContent/NavBar/Logo/Logo.tsx
@@ -2,17 +2,18 @@ import { useMatch } from 'react-router-dom';
 
 import { LogoSvg } from 'assets/svg';
 import Link from 'components/Link';
+import { greyColors, redColors } from 'theme/colors';
 
 const Logo: React.FC = () => {
   const isRootPath = !!useMatch('/');
 
   if (isRootPath) {
-    return <LogoSvg style={{ display: 'inline-block' }} />;
+    return <LogoSvg style={{ display: 'inline-block', fill: greyColors.light }} />;
   }
 
   return (
     <Link to='/'>
-      <LogoSvg style={{ display: 'inline-block' }} />
+      <LogoSvg style={{ display: 'inline-block', fill: redColors.dark }} />
     </Link>
   );
 };
