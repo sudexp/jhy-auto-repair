2370eb88b0e802be2a5f8a210652df63e355457b
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 01:18:53 +0200

    svg fill colors

--- src/pages/Services/utils.tsx
+++ src/pages/Services/utils.tsx
@@ -21,6 +21,7 @@ import {
   ExhaustPipeSvg,
   TransmissionSvg,
 } from 'assets/svg/service-icons';
+import { typographyColors } from 'theme/colors';
 
 interface Service {
   id: number;
@@ -32,96 +33,96 @@ export const services: Service[] = [
   {
     id: 0,
     title: 'Rengasmyynti ja Rengashotelli, kausisäilytys',
-    component: <WheelSvg />,
+    component: <WheelSvg fill={typographyColors.dark} />,
   },
   {
     id: 1,
     title: 'Sijaisautot huollon tai korjauksen ajaksi',
-    component: <ChangeVehicleSvg />,
+    component: <ChangeVehicleSvg fill={typographyColors.dark} />,
   },
   {
     id: 2,
     title: 'Henkilö- ja pakettiautojen kaikki huollot ja korjaukset',
-    component: <ServiceSvg />,
+    component: <ServiceSvg fill={typographyColors.dark} />,
   },
   {
     id: 3,
     title: 'Ilmastointi huolto ja korjaus',
-    component: <ConditioningSvg />,
+    component: <ConditioningSvg fill={typographyColors.dark} />,
   },
   {
     id: 4,
     title: 'Renkaiden tasapainotus ja 3D-suuntaus',
-    component: <TireBalancingSvg />,
+    component: <TireBalancingSvg fill={typographyColors.dark} />,
   },
   {
     id: 5,
     title: 'Vikadiagnostiikka ja raportointi',
-    component: <DiagnosisSvg />,
+    component: <DiagnosisSvg fill={typographyColors.dark} />,
   },
   {
     id: 6,
     title: 'Määräaikaishuollot – Autosi takuu säilyy',
-    component: <MaintenanceWarrantySvg />,
+    component: <MaintenanceWarrantySvg fill={typographyColors.dark} />,
   },
   {
     id: 7,
     title: 'Ruostevauriokorjaukset',
-    component: <RustSvg />,
+    component: <RustSvg fill={typographyColors.dark} />,
   },
   {
     id: 8,
     title: 'Sisätilalämmittimet',
-    component: <HeaterSvg />,
+    component: <HeaterSvg fill={typographyColors.dark} />,
   },
   {
     id: 9,
     title: 'Päästömittaukset ja diagnostiikat',
-    component: <EmissionSvg />,
+    component: <EmissionSvg fill={typographyColors.dark} />,
   },
   {
     id: 10,
     title: 'Henkilöauton ja pakettiauton käynnistimet, akut, laturit',
-    component: <BatterySvg />,
+    component: <BatterySvg fill={typographyColors.dark} />,
   },
   {
     id: 11,
     title: 'Öljynvaihdot',
-    component: <OilChangeSvg />,
+    component: <OilChangeSvg fill={typographyColors.dark} />,
   },
   {
     id: 12,
     title: 'Jakopää huollot- ja korjaukset',
-    component: <EngineSvg />,
+    component: <EngineSvg fill={typographyColors.dark} />,
   },
   {
     id: 13,
     title: 'Jäähdytysjärjestelmän huollot- ja korjaukset',
-    component: <CoolingSvg />,
+    component: <CoolingSvg fill={typographyColors.dark} />,
   },
   {
     id: 14,
     title: 'Ohjaus- ja jousitus työt',
-    component: <SuspensionSvg />,
+    component: <SuspensionSvg fill={typographyColors.dark} />,
   },
   {
     id: 15,
     title: 'Jarrujen huollot- ja korjaukset',
-    component: <BrakesSvg />,
+    component: <BrakesSvg fill={typographyColors.dark} />,
   },
   {
     id: 16,
     title: 'Polttoainejarjestelmien huollot sekä korjaukset',
-    component: <FuelSvg />,
+    component: <FuelSvg fill={typographyColors.dark} />,
   },
   {
     id: 17,
     title: 'Pakokaasujärjestelmien korjaustyöt',
-    component: <ExhaustPipeSvg />,
+    component: <ExhaustPipeSvg fill={typographyColors.dark} />,
   },
   {
     id: 18,
     title: 'Voimansiirron huollot ja korjaukset',
-    component: <TransmissionSvg />,
+    component: <TransmissionSvg fill={typographyColors.dark} />,
   },
 ];
