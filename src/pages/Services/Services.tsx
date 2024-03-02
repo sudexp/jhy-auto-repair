e87e6fb16b25791148a42a09c5ab40c075462174
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 00:48:14 +0200

    grid system

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,39 +1,44 @@
-import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Stack, Typography, useTheme } from '@mui/material';
+import { Avatar, Box, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from '@mui/material';
 
 import Seo from 'components/Seo';
 
 import { services } from './utils';
 
 const Services: React.FC = () => {
-  const theme = useTheme();
-
   return (
     <>
       <Seo title='Jyväskylän Huoltoykköset - Palvelut' description='' name='' type='' />
       <Stack id='services' spacing={3}>
         <Typography variant='h4'>TÄYDEN PALVELUN AUTOHUOLTO KUSTANNUSTEHOKKAASTI</Typography>
-        <Box>
-          <List sx={{ width: '100%', maxWidth: theme.breakpoints.values.md }}>
-            {services.map(({ id, title, component }) => {
-              return (
-                <ListItem key={id}>
-                  <ListItemAvatar>
-                    <Avatar
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        bgcolor: 'transparent',
-                      }}
-                    >
-                      {component}
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary={title} />
-                </ListItem>
-              );
-            })}
-          </List>
+        <Box
+          display='grid'
+          gridTemplateColumns='repeat(2, 1fr)'
+          gap={3}
+          sx={{
+            '@media (max-width: 600px)': {
+              gridTemplateColumns: 'repeat(1, 1fr)',
+            },
+          }}
+        >
+          {services.map(({ id, title, component }) => (
+            <Box key={id} gridColumn='span 1'>
+              <ListItem disablePadding>
+                <ListItemAvatar>
+                  <Avatar
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      bgcolor: 'transparent',
+                    }}
+                  >
+                    {component}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={title} />
+              </ListItem>
+            </Box>
+          ))}
         </Box>
         <Typography variant='p1'>… ja kaikki muu mahdollinen autosi hyvinvoinnin tueksi!</Typography>
       </Stack>
