075477d250acf68ef23aabd37043b318cdf63de3
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 15:16:01 +0200

    rounded photos

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, useTheme } from '@mui/material';
+import { Avatar, Box, Stack, Typography, useTheme } from '@mui/material';
 
 import Seo from 'components/Seo';
 import LeafletMap from 'components/LeafletMap';
@@ -11,23 +11,27 @@ const Contacts: React.FC = () => {
       <Seo title='Jyväskylän Huoltoykköset - Yhteystiedot' description='' name='' type='' />
       <Box sx={{}}>
         <Stack direction='row' spacing={4} sx={{ justifyContent: 'center' }}>
-          <Stack spacing={1}>
-            <Box
-              component='img'
-              sx={{ display: 'block', maxHeight: '200px', maxWidth: '100%' }}
+          <Stack spacing={1} sx={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Avatar
               alt='Asentaja1'
-              src='/src/assets/png/photo1.png'
+              src='src/assets/png/photo1.png'
+              sx={{
+                width: '150px',
+                height: '150px',
+              }}
             />
             <Typography variant='p2' color='typography.dark'>
-              Asentaja Andrei
+              Asentaja Andrey
             </Typography>
           </Stack>
-          <Stack spacing={1}>
-            <Box
-              component='img'
-              sx={{ display: 'block', maxHeight: '200px', maxWidth: '100%', position: 'relative' }}
-              alt='Asentaja2'
-              src='/src/assets/png/photo2.png'
+          <Stack spacing={1} sx={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Avatar
+              alt='Asentaja1'
+              src='src/assets/png/photo2.png'
+              sx={{
+                width: '150px',
+                height: '150px',
+              }}
             />
             <Typography variant='p2' color='typography.dark'>
               Asentaja Aaro
