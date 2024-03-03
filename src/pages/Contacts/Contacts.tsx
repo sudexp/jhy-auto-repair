33a5585fd28c41c73d7883ffc5816a1a7eeaf618
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 17:01:59 +0200

    content added

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Stack, Typography, useTheme } from '@mui/material';
+import { Avatar, Box, Link, Stack, Typography, useTheme } from '@mui/material';
 
 import Seo from 'components/Seo';
 import LeafletMap from 'components/LeafletMap';
@@ -9,38 +9,63 @@ const Contacts: React.FC = () => {
   return (
     <>
       <Seo title='Jyväskylän Huoltoykköset - Yhteystiedot' description='' name='' type='' />
-      <Box sx={{}}>
-        <Stack direction='row' spacing={4} sx={{ justifyContent: 'center' }}>
-          <Stack spacing={1} sx={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Avatar
-              alt='Asentaja1'
-              src='src/assets/png/photo1.png'
-              sx={{
-                width: '150px',
-                height: '150px',
-              }}
-            />
-            <Typography variant='p2' color='typography.dark'>
-              Asentaja Andrey
-            </Typography>
+      <Box sx={{ '&': { color: 'typography.dark' } }}>
+        <Stack spacing={2}>
+          <Typography variant='h4'>YHTEYSTIEDOT: Kokemus ja ammattitaito jokaisessa yksityiskohdassa</Typography>
+          <Typography>
+            Tervetuloa tämänsä sivuun! On iloa esittää teille meidän kokeneen ja ammattitaitavasti koulutetun henkilöstön. Meidän
+            tavoitteemme on tarjota teille korkealaatuisen autonhuollon palveluita, jotka perustuvat meidän yli kymmenen vuoden kokemuksesta
+            ja ammattitaitoon.
+          </Typography>
+          <Typography>
+            Meidän maisterimme erottuu korkealta koulutukselta ja ammattitaitolta, mikä mahdollistaa meille tarjota teille erityislaatuisen
+            palvelun. Meidän työskentelyssä on aina huomiota herättävän tarkka ja huolellinen lähestymistapa, joka takaa teille parhaan
+            mahdollisen tuloksen.
+          </Typography>
+          <Typography>
+            Olemme avoimia arkisin maanantaita pe klo 8–16.30 osoitteessa Kuormaajantie 6, 40320, Jyväskylä. Ota yhteyttä meille puhelimitse{' '}
+            <Link href='tel:0452393591' underline='hover' color='typography.dark'>
+              0452393591
+            </Link>{' '}
+            tai varaa aikaa.
+          </Typography>
+          <Typography>Meidän tiimimme:</Typography>
+          <Stack direction='row' spacing={4}>
+            <Stack spacing={1} sx={{ alignItems: 'center' }}>
+              <Avatar
+                alt='Asentaja1'
+                src='src/assets/png/photo1.png'
+                sx={{
+                  width: '150px',
+                  height: '150px',
+                }}
+              />
+              <Typography variant='p2'>Andrey</Typography>
+            </Stack>
+            <Stack spacing={1} sx={{ alignItems: 'center' }}>
+              <Avatar
+                alt='Asentaja1'
+                src='src/assets/png/photo2.png'
+                sx={{
+                  width: '150px',
+                  height: '150px',
+                }}
+              />
+              <Typography variant='p2'>Aaro</Typography>
+            </Stack>
           </Stack>
-          <Stack spacing={1} sx={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Avatar
-              alt='Asentaja1'
-              src='src/assets/png/photo2.png'
-              sx={{
-                width: '150px',
-                height: '150px',
-              }}
-            />
-            <Typography variant='p2' color='typography.dark'>
-              Asentaja Aaro
-            </Typography>
-          </Stack>
+          <Typography>
+            Olemme iloisia näkemään teidät meidän maisterimme ja tarjoamme teille turvallisen ja miellyttävän palveluaseman.
+          </Typography>
+          <Typography>
+            Löydä meidät helposti Seppäänkangas-alueella! Tämä sijainti tekee meistä täydellisen valinnan sekä koti- että työmatkailijoille
+            etsien korkealaatuisen autonhuollon palveluita lähellä.
+          </Typography>
         </Stack>
         <Box id='contacts' sx={{ maxWidth: theme.breakpoints.values.lg, margin: '0 auto' }}>
           <LeafletMap />
         </Box>
+        <Typography>Odottamme sinua maisterimme Seppäänkangas-alueella ja tarjoamme sinulle meidän autonhuollon palveluita.</Typography>
       </Box>
     </>
   );
