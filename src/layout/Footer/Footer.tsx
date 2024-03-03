d779a91e61878af382b8f637c6dee21a98916e7d
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 15:46:45 +0200

    main content added

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { useMatch } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, Link, Stack, Typography, useTheme } from '@mui/material';
 
 import LayoutContainer from 'components/LayoutContainer';
 import Logo from 'layout/NavBarWithContent/NavBar/Logo';
@@ -8,11 +8,64 @@ import { getBgColor } from './utils';
 
 const Footer: React.FC = () => {
   const isRootPath = !!useMatch('/');
+  const theme = useTheme();
 
   return (
     <Box sx={{ display: 'flex', flexShrink: 0 }}>
       <LayoutContainer sx={{ py: { xs: 2, md: 4 }, bgcolor: getBgColor(isRootPath) }}>
-        <Logo />
+        <Box
+          display='grid'
+          gridTemplateColumns='repeat(2, 1fr)'
+          gap={3}
+          sx={{
+            [`@media (max-width: ${theme.breakpoints.values.sm}px)`]: {
+              gridTemplateColumns: 'repeat(1,  1fr)',
+            },
+          }}
+        >
+          <Box gridColumn='span 1'>
+            <Logo />
+          </Box>
+          <Box
+            gridColumn='span 1'
+            sx={{
+              '&': {
+                color: 'typography.light',
+              },
+            }}
+          >
+            <Stack direction='row' spacing={1}>
+              <Typography>Osoite:</Typography>
+              <Typography>Kuormaajantie 6, 40320, Jyväskylä</Typography>
+            </Stack>
+            <Stack direction='row' spacing={1}>
+              <Typography>Puhelinnumero:</Typography>
+              <Link href='tel:0452393591' underline='hover' color='typography.light'>
+                0452393591
+              </Link>
+            </Stack>
+            <Stack direction='row' spacing={1}>
+              <Typography>Sähköposti:</Typography>
+              <Typography>
+                <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color='typography.light'>
+                  huoltoykkoset@gmail.com
+                </Link>
+              </Typography>
+            </Stack>
+            <Stack direction='row' spacing={1}>
+              <Typography>Laskut:</Typography>
+              <Typography>
+                <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color='typography.light'>
+                  huoltoykkoset.laskutus@gmail.com
+                </Link>
+              </Typography>
+            </Stack>
+            <Stack direction='row' spacing={1}>
+              <Typography>Y-tunnus</Typography>
+              <Typography>3422467-8</Typography>
+            </Stack>
+          </Box>
+        </Box>
       </LayoutContainer>
     </Box>
   );
