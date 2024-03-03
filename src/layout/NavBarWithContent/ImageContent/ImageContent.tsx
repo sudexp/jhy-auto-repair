543f7d2c4b04762be39ec7a48196255910dfa1ae
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-03 16:15:14 +0200

    content modified

--- src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
+++ src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
@@ -4,25 +4,28 @@ import PhoneLink from 'components/PhoneLink';
 
 const ImageContent: React.FC = () => {
   return (
-    <Stack spacing={4}>
-      <Typography variant='h2' color='typography.light'>
-        JYVÄSKYLÄN HUOLTOYKKÖSET
+    <Stack
+      spacing={4}
+      sx={{
+        '&': {
+          color: 'typography.light',
+        },
+      }}
+    >
+      <Typography variant='h2'>JYVÄSKYLÄN HUOLTOYKKÖSET</Typography>
+      <Typography variant='ui2r'>Tämä on teidän luottamukselliset autonhuollon kumppanit!</Typography>
+      <Typography variant='ui2r'>
+        Tervetuloa paremman autohuollon asiakkaaksi, jossa tarjoamme teille korkealaatuisen autonhuollon palveluita, jotka perustuvat yli
+        kymmenen vuoden kokemuksesta ja ammattitaitoon. Meidän tarkoituksemme on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja
+        tarjota ne hinta-arvoparissa.
       </Typography>
-      <Typography variant='ui2r' color='typography.light'>
-        Jyväskylän huoltoykköset Oy palveluksessasi.
-      </Typography>
-      <Typography variant='ui2r' color='typography.light'>
-        Tervetuloa paremman autohuollon asiakkaaksi, jossa ajatelemme kuskin kukkaron ja auton parasta yli kymmenen vuoden kokemuksella ja
-        hintatakuulla tottakai.
-      </Typography>
-      <Typography variant='ui2r' color='typography.light'>
-        Palvelemme arkisin ma–pe 8-16.30 osoiteessa Kuormaajantie 6, 40320, Jyväskylä
+      <Typography variant='ui2r'>
+        Olemme avoimia arkisin maanantaita ma–pe klo 8–16.30 osoitteessa Kuormaajantie 6, 40320, Jyväskylä.
       </Typography>
       <Stack direction='row' spacing={1}>
-        <Typography variant='ui3r' color='typography.light'>
-          Soita ja varaa aikaa:
-        </Typography>
+        <Typography variant='ui2r'>Ota yhteyttä meille puhelimitse</Typography>
         <PhoneLink phoneNumber='0452393591' />
+        <Typography variant='ui2r'>tai varaa aikaa.</Typography>
       </Stack>
     </Stack>
   );
