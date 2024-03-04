9c7194440daff3d45b2f29671e13dff0fe393ab7
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-04 14:00:24 +0200

    ImageContent modified

--- src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
+++ src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
@@ -20,17 +20,17 @@ const ImageContent: React.FC = () => {
       <Typography variant='h1'>JYVÄSKYLÄN HUOLTOYKKÖSET</Typography>
       <Typography variant='p2b'>Tämä on teidän luottamukselliset autonhuollon kumppanit!</Typography>
       <Typography variant='p2'>
-        Tervetuloa paremman autohuollon asiakkaaksi, jossa tarjoamme teille korkealaatuisen autonhuollon palveluita, jotka perustuvat yli
-        kymmenen vuoden kokemuksesta ja ammattitaitoon. Meidän tarkoituksemme on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja
-        tarjota ne hinta-arvoparissa.
+        Tervetuloa paremman autohuollon asiakkaaksi. Tarjoamme teille korkealaatuisia autonhuollon palveluita, jotka perustuvat yli kymmenen
+        vuoden kokemukseen ja ammattitaitoon. Meidän tarkoituksemme on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja tarjota ne
+        edulisesti.
       </Typography>
       <Stack direction='row' spacing={1}>
         <Typography variant='p2'>
-          Olemme avoimia arkisin maanantaita ma–pe klo 8–16.30 osoitteessa <b>Kuormaajantie 6, 40320, Jyväskylä</b>.
+          Olemme avoimia arkisin ma–pe klo 8–16.30 osoitteessa <b>Kuormaajantie 6, 40320, Jyväskylä</b>.
         </Typography>
       </Stack>
       <Stack direction={getDirection(isSmDown)} spacing={1} sx={{ alignItems: getAlignItems(isSmDown) }}>
-        <Typography variant='p2'>Ota yhteyttä meille puhelimitse tai varaa aikaa: </Typography>
+        <Typography variant='p2'>Ota yhteyttä meille puhelimitse: </Typography>
         <PhoneLink phoneNumber='0452393591' />
       </Stack>
     </Stack>
