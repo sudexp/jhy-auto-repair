53276881eb6c3326f12d7db9996cf2583d317f87
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-06 16:18:06 +0200

    footer content as a styled table

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,29 @@
+import { ReactNode } from 'react';
 import { useMatch, Link as RouterLink } from 'react-router-dom';
-import { Box, Link, Stack, Typography, useTheme } from '@mui/material';
+import { Box, Link, Table, TableBody, TableCell, TableContainer, TableRow, Typography, useTheme } from '@mui/material';
 
 import LayoutContainer from 'components/LayoutContainer';
 import Logo from 'layout/NavBarWithContent/NavBar/Logo';
 
 import { getBgColor } from './utils';
+
+interface TableCellProps {
+  children: ReactNode;
+}
+
+const color = 'typography.light';
+
+const LeftTableCell: React.FC<TableCellProps> = ({ children }: TableCellProps) => (
+  <TableCell sx={{ borderBottom: 'none', color, p: 0, opacity: 0.85 }}>
+    <Typography variant='p3'>{children}</Typography>
+  </TableCell>
+);
+
+const RightTableCell: React.FC<TableCellProps> = ({ children }: TableCellProps) => (
+  <TableCell sx={{ borderBottom: 'none', color, p: 0, pl: 2 }}>
+    <Typography variant='p3'>{children}</Typography>
+  </TableCell>
+);
 
 const Footer: React.FC = () => {
   const isRootPath = !!useMatch('/');
@@ -26,59 +45,78 @@ const Footer: React.FC = () => {
           <Box gridColumn='span 1'>
             <Logo />
           </Box>
-          <Box
-            gridColumn='span 1'
-            sx={{
-              '&': {
-                color: 'typography.light',
-              },
-            }}
-          >
-            <Stack direction='row' spacing={1}>
-              <Typography variant='p3'>Osoite:</Typography>
-              <Typography
-                variant='p3'
-                sx={{
-                  '& a': {
-                    color: 'typography.light',
-                    textDecoration: 'none',
-                    '&:hover': {
-                      textDecoration: 'underline',
-                    },
-                  },
-                }}
-              >
-                <RouterLink to='/contacts'>Kuormaajantie 6, 40320, Jyväskylä</RouterLink>
-              </Typography>
-            </Stack>
-            <Stack direction='row' spacing={1}>
-              <Typography variant='p3'>Puhelinnumero:</Typography>
-              <Typography variant='p3'>
-                <Link href='tel:0452393591' underline='hover' color='typography.light'>
-                  0452393591
-                </Link>
-              </Typography>
-            </Stack>
-            <Stack direction='row' spacing={1}>
-              <Typography variant='p3'>Sähköposti:</Typography>
-              <Typography variant='p3'>
-                <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color='typography.light'>
-                  huoltoykkoset@gmail.com
-                </Link>
-              </Typography>
-            </Stack>
-            <Stack direction='row' spacing={1}>
-              <Typography variant='p3'>Laskut:</Typography>
-              <Typography variant='p3'>
-                <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color='typography.light'>
-                  huoltoykkoset.laskutus@gmail.com
-                </Link>
-              </Typography>
-            </Stack>
-            <Stack direction='row' spacing={1}>
-              <Typography variant='p3'>Y-tunnus:</Typography>
-              <Typography variant='p3'>3422467-8</Typography>
-            </Stack>
+          <Box gridColumn='span 1'>
+            <TableContainer>
+              <Table size='small' aria-label='contact-information' sx={{ width: 'auto' }}>
+                <TableBody>
+                  <TableRow>
+                    <LeftTableCell>
+                      <Typography variant='p3'>Osoite:</Typography>
+                    </LeftTableCell>
+                    <RightTableCell>
+                      <Typography
+                        variant='p3'
+                        sx={{
+                          '& a': {
+                            color,
+                            textDecoration: 'none',
+                            '&:hover': {
+                              textDecoration: 'underline',
+                            },
+                          },
+                        }}
+                      >
+                        <RouterLink to='/contacts'>Kuormaajantie 6, 40320, Jyväskylä</RouterLink>
+                      </Typography>
+                    </RightTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <LeftTableCell>
+                      <Typography variant='p3'>Puhelinnumero:</Typography>
+                    </LeftTableCell>
+                    <RightTableCell>
+                      <Typography variant='p3'>
+                        <Link href='tel:0452393591' underline='hover' color={color}>
+                          0452393591
+                        </Link>
+                      </Typography>
+                    </RightTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <LeftTableCell>
+                      <Typography variant='p3'>Sähköposti:</Typography>
+                    </LeftTableCell>
+                    <RightTableCell>
+                      <Typography variant='p3'>
+                        <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color={color}>
+                          huoltoykkoset@gmail.com
+                        </Link>
+                      </Typography>
+                    </RightTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <LeftTableCell>
+                      <Typography variant='p3'>Laskut:</Typography>
+                    </LeftTableCell>
+                    <RightTableCell>
+                      <Typography variant='p3'>
+                        <Link href='mailto:huoltoykkoset@gmail.com' underline='hover' color={color}>
+                          huoltoykkoset.laskutus@gmail.com
+                        </Link>
+                      </Typography>
+                    </RightTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <LeftTableCell>
+                      <Typography variant='p3'>Y-tunnus:</Typography>
+                    </LeftTableCell>
+                    <RightTableCell>
+                      <Typography variant='p3'>3422467-8</Typography>
+                    </RightTableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
         </Box>
       </LayoutContainer>
