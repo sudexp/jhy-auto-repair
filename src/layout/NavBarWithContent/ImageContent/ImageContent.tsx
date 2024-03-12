711451caf3474c216de1038772046d7adeb01302
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-12 20:48:11 +0200

    address & phone to the bottom

--- src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
+++ src/layout/NavBarWithContent/ImageContent/ImageContent.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Divider, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
 
 import PhoneLink from 'components/PhoneLink';
 
@@ -10,27 +10,38 @@ const ImageContent: React.FC = () => {
 
   return (
     <Stack
-      spacing={4}
+      spacing={2}
       sx={{
         '&': {
+          justifyContent: 'space-between',
+          height: 'calc(100% - 100px)', // 80px (mobile)/ 88px(desktop) is the height of the navbar
           color: 'typography.light',
         },
       }}
     >
-      <Typography variant='h1'>JYVÄSKYLÄN HUOLTOYKKÖSET</Typography>
-      <Typography variant='p2b'>Luottamuksellinen autonhuollon kumppanisi!</Typography>
-      <Typography variant='p2'>
-        Tervetuloa paremman autohuollon asiakkaaksi. Tarjoamme teille korkealaatuisia autonhuollon palveluita, jotka perustuvat yli kymmenen
-        vuoden kokemukseen ja ammattitaitoon. Tarkoituksemme on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja tarjota ne
-        edullisesti.
-      </Typography>
-      <Typography variant='p2'>
-        Palvelemme ma–pe klo 8:00–16.30 osoitteessa <b>Kuormaajantie 6, 40320, Jyväskylä</b>.
-      </Typography>
-      <Stack direction={getDirection(isSmDown)} spacing={1} sx={{ alignItems: getAlignItems(isSmDown) }}>
-        <Typography variant='p2'>Ota yhteyttä meille puhelimitse: </Typography>
-        <PhoneLink phoneNumber='0452393591' />
+      <Stack spacing={1}>
+        <Stack spacing={1}>
+          <Typography variant='h1'>JYVÄSKYLÄN HUOLTOYKKÖSET</Typography>
+          <Divider sx={{ backgroundColor: 'typography.light' }} />
+        </Stack>
+        <Stack spacing={1}>
+          <Typography variant='p2b'>Luottamuksellinen autonhuollon kumppanisi!</Typography>
+          <Typography variant='p2'>
+            Tervetuloa paremman autohuollon asiakkaaksi. Tarjoamme teille korkealaatuisia autonhuollon palveluita, jotka perustuvat yli
+            kymmenen vuoden kokemukseen ja ammattitaitoon. Tarkoituksemme on auttaa teitä löytämään parhaat autonhuollon ratkaisut ja
+            tarjota ne edullisesti.
+          </Typography>
+        </Stack>
       </Stack>
+      <Box>
+        <Typography variant='p2'>
+          Palvelemme ma–pe klo 8:00–16.30 osoitteessa <b>Kuormaajantie 6, 40320, Jyväskylä</b>.
+        </Typography>
+        <Stack direction={getDirection(isSmDown)} spacing={1} sx={{ alignItems: getAlignItems(isSmDown) }}>
+          <Typography variant='p2'>Ota yhteyttä meille puhelimitse: </Typography>
+          <PhoneLink phoneNumber='0452393591' />
+        </Stack>
+      </Box>
     </Stack>
   );
 };
