915ec5c57d7e511e01440efe5319d3a01bf82db6
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-03-20 12:24:34 +0200

    Root seo modified

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -4,8 +4,8 @@ const Root: React.FC = () => {
   return (
     <Seo
       title='Jyväskylän Huoltoykköset - Etusivu'
-      description='Korjaamo Jyväskylä, autohuolto Jyväskylä, autopalvelu Jyväskylä, rengasmyynti ja rengashotelli, kausisäilytys Jyväskylä, henkilö- ja pakettiautojen kaikki huollot ja korjaukset Jyväskylä, renkaiden tasapainotus ja 3D-suuntaus Jyväskylä, määräaikaishuollot – autosi takuu säilyy Jyväskylä, sisätilalämmittimet Jyväkylä, henkilöauton ja pakettiauton käynnistimet, akut, laturit Jyväkylä, Jakopää huollot- ja korjaukset Jyväskylä, Ohjaus- ja jousitus työt Jyväskylä, Polttoainejarjestelmien huollot sekä korjaukset Jyväskylä, Voimansiirron huollot ja korjaukset Jyväskylä, sijaisautot huollon tai korjauksen ajaksi Jyväskylä, Ilmastointi huolto ja korjaus Jyväskylä, vikadiagnostiikka ja raportointi Jyväskylä, ruostevauriokorjaukset Jyväskylä, päästömittaukset ja diagnostiikat Jyväskylä, öljynvaihdot Jyväskylä, jäähdytysjärjestelmän huollot- ja korjaukset Jyväskylä, jarrujen huollot- ja korjaukset Jyväskylä, pakokaasujärjestelmien korjaustyöt Jyväskylä'
-      name='Jyväskylän Huoltoykköset - Palvelut'
+      description='Edullinen korjaamo Jyväskylä, autohuolto Jyväskylä, autopalvelu Jyväskylä, Jyväskylän Huoltoykköset'
+      name='Jyväskylän Huoltoykköset'
       type='website'
     />
   );
