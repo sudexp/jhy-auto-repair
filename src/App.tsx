f62fa02e848876046e56854f772766a8c7e51907
Dmitry Sklyarov <dmitry.sklyarov@movya.fi>
2024-07-21 23:12:04 +0300

    google analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { ThemeProvider } from '@mui/system';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 import theme from 'theme';
 import { router } from 'router';
 
 const App = () => {
+  ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+
   return (
     <ThemeProvider theme={theme}>
       <StyledEngineProvider injectFirst>
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,6 @@
-import { useMatch } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useMatch, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import { Stack } from '@mui/material';
 
 import Footer from './Footer';
@@ -7,6 +9,11 @@ import { getSpacing } from './utils';
 
 const Layout: React.FC = () => {
   const isRootPath = !!useMatch('/');
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: pathname });
+  }, [pathname]);
 
   return (
     <Stack spacing={getSpacing(isRootPath)} sx={{ minHeight: '100vh' }}>
